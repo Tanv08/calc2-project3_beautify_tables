a39b7c7cb4a6d1efe87d66377328ead35155c20e
tanv08 <tk362@njit.edu>
2021-12-13 16:34:31 -0500

    updated python files

--- calc/calculations/division.py
+++ calc/calculations/division.py
@@ -1,23 +1,13 @@
 """Division Class"""
-import pprint
 
 from calc.calculations.calculation import Calculation
+
 
 class Division(Calculation):
     """division calculation object"""
     def get_result(self):
         """get the division results"""
-        initial = 1.0
-        result = 0
-        for value in self.values:
-            pprint.pprint(value)
-            pprint.pprint(result)
-            if result == 0:
-                result = value / initial
-            else:
-                try:
-                    result = result / value
-                except ZeroDivisionError:
-                    return 0.0
-            pprint.pprint(result)
-        return result
+        result = self.values[0]
+        for value in self.values[1:]:
+            result = result / value
+            return result
